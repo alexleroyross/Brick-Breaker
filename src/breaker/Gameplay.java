0ac2f1ac0e9a7297be2252e63e3c445e8b704ea0
Frozelar <frostyfish88@gmail.com>
2018-12-30 17:04:58 -0500

    Created Project Skeleton
    - Outlined basic functionality in Main and Gameplay classes

--- src/breaker/Gameplay.java
+++ src/breaker/Gameplay.java
@@ -1,5 +1,47 @@
 package breaker;
 
-public class Gameplay {
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+	public Gameplay()
+	{
+		
+	}
+	
+	public void DrawGraphics(Graphics g)
+	{
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- src/breaker/Main.java
+++ src/breaker/Main.java
@@ -1,10 +1,22 @@
 package breaker;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame obj = new JFrame();
+		Gameplay gameplay = new Gameplay();
+		
 
+		obj.setBounds(20, 20, 800, 600);
+		obj.setBackground(Color.white);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
 	}
 
 }
